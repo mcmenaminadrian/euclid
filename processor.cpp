bd2bbb717672b92b3ddc69f41f5fd8c32781f0d8
Adrian McMenamin <acm538@york.ac.uk>
2019-03-18 22:45:44 +0000

    fix stack pointer issue

--- processor.cpp
+++ processor.cpp
@@ -1016,18 +1016,18 @@ void Processor::waitGlobalTick()
 void Processor::pushStackPointer()
 {
 	stackPointer -= sizeof(uint64_t);
-    if (stackPointer >= stackPointerUnder) {
-        cerr << "Stack Underflow" << endl;
-        throw "Stack Underflow\n";
+	if (stackPointer < stackPointerOver) {
+        	cerr << "Stack Underflow" << endl;
+        	throw "Stack Underflow\n";
 	}
 }
 
 void Processor::popStackPointer()
 {
 	stackPointer += sizeof(uint64_t);
-    if (stackPointer < stackPointerOver) {
-        cerr << "Stack Overflow" << endl;
-        throw "Stack Overflow\n";
+	if (stackPointer > stackPointerUnder) {
+        	cerr << "Stack Overflow" << endl;
+        	throw "Stack Overflow\n";
 	}
 }
 
